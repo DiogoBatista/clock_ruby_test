cffd9cda0f7028aa85ed660f859094dcb63d4f88
Diogo Batista <diogobv.batista@gmail.com>
2020-02-21 10:10:03 +0100

    test_on_the_hour

--- clock.rb
+++ clock.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 class Clock
+  attr_reader :hour
+
+  def initialize(hour: 0)
+    @minutes = hour * 60
+  end
+
+  def to_s
+    format('%02i:00', time)
+  end
+
+  def time
+    @minutes / 60
+  end
 end
