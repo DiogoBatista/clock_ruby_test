e35e110b75b616be85638304412a3353aa9431de
Diogo Batista <diogobv.batista@gmail.com>
2020-02-21 10:16:45 +0100

    test_past_the_hour add minutes into account

--- clock.rb
+++ clock.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class Clock
-  attr_reader :hour
+  MINUTES_IN_HOUR = 60
+  HOURS_IN_DAY = 24
+  attr_reader :hour, :minute
 
-  def initialize(hour: 0)
-    @minutes = hour * 60
+  def initialize(hour: 0, minute: 0)
+    @minutes = hour * 60 + minute
   end
 
   def to_s
-    format('%02i:00', time)
+    format('%02i:%02i', *time)
   end
 
   def time
-    @minutes / 60
+    [@minutes / MINUTES_IN_HOUR % HOURS_IN_DAY, @minutes % MINUTES_IN_HOUR]
   end
 end
